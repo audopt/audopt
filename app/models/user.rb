ef39afb16e183e6a07cc459e0391755f4733f000
Victor Navarro <victor.matias.navarro@gmail.com>
2016-10-01 07:01:32 -0300

    Adding avatars to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ActiveRecord::Base
 	validates :birth, presence: true
 
 	has_secure_password
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  has_many :reports
 
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
