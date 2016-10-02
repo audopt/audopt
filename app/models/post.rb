dfa2f435ce676a955e47b50adcae9691cebf7283
Victor Navarro <victor.matias.navarro@gmail.com>
2016-10-02 11:41:59 -0300

    Refactoring views

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ActiveRecord::Base
   has_many :interest_posts
   has_many :interested_by, through: :interest_posts, source: :user
   has_one :animal
-
-  validates :text, presence: true, length: { maximum: 240 }
-  validates :location, presence: true, length: { maximum: 240 }
+	validates :location, presence: true, length: { maximum: 240 }
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
